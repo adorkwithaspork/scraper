0f9b4c8124376e4eb0a978e50fef47b9749a47f4
Grace Andrizzi <graceandrizzi@graces-mbp.lan>
2018-09-26 18:58:24 -0700

    adding server changes

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ var db = require("./models");
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_86k0vlt6:dpukep8jnddetf7np0hq9uuqkv@ds115753.mlab.com:15753/heroku_86k0vlt6";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 // Use body-parser for handling form submissions
